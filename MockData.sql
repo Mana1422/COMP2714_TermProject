190418caed2baa77640fea4c6adc7952323e52c3
Chunki Lam <clam193@my.bcit.ca>
2024-11-28 13:20:24 -0800

    Updated insert statements to meet the constraints.

--- MockData.sql
+++ MockData.sql
@@ -1,12 +1,11 @@
-INSERT INTO Athlete (ID, FirstName, MiddleName, LastName, WeightKg, Sex, Birthday, CountryName, SportID, AccommodationName, RoomNo)
+INSERT INTO Athlete (ID, FirstName, LastName, WeightKg, Sex, Birthday, CountryName, SportID, AccommodationName, RoomNumber, TeamName)
 VALUES
-(1, 'John', 'M.', 'Doe', 75.5, 'Male', '1990-05-12', 'USA', 101, 'Olympic Village', 202),
-(2, 'Anna', 'L.', 'Smith', 63.7, 'Female', '1992-09-22', 'Canada', 102, 'Athletes Inn', 105),
-(3, 'Liu', NULL, 'Wang', 68.0, 'Male', '1988-07-01', 'China', 103, 'Olympic Village', 302),
-(4, 'Maria', 'T.', 'Garcia', 56.3, 'Female', '1994-11-15', 'Mexico', 104, 'Athletes Inn', 110);
+(1, 'John', 'Doe', 75.5, 'Male', '1990-05-12', 'USA', 101, 'Olympic Village', 202, 'Canada'),
+(2, 'Anna', 'Smith', 63.7, 'Female', '1992-09-22', 'Canada', 102, 'Athletes Inn', 105, 'USA'),
+(3, 'Liu', 'Wang', 68.0, 'Male', '1988-07-01', 'China', 103, 'Olympic Village', 302, 'China'),
+(4, 'Maria', 'Garcia', 56.3, 'Female', '1994-11-15', 'Mexico', 104, 'Athletes Inn', 110, 'Mexico');
 
-
-INSERT INTO Country (countryName, NumGolds, NumSilver, NumBronze)
+INSERT INTO Country (Name, NumGolds, NumSilver, NumBronze)
 VALUES
 ('USA', 25, 30, 20),
 ('Canada', 15, 20, 25),
@@ -22,7 +21,7 @@ VALUES
 (4, 'Clark', 'Kent', '555-765-4321');
 
 
-INSERT INTO Salaried (SalariedID)
+INSERT INTO Salaried (StaffID)
 VALUES
 (1),
 (2);
@@ -41,16 +40,23 @@ VALUES
 ('Coca-Cola', 'Beverages', 456789123);
 
 
+INSERT INTO Event (BeginTimeStamp, EndTimeStamp, VenueName, SportID)
+VALUES
+('2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium', 101),
+('2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center', 102),
+('2024-07-23 18:00:00', '2024-07-23 20:00:00', 'Gymnastics Arena', 104);
+
+
 INSERT INTO SalariedStaffs (SalariedStaffID, EventBeginTimeStamp, EventEndTimeStamp, EventVenueName, HourlyWageEuros, HoursWorked, Role)
 VALUES
-(1, '08:00:00', '12:00:00', 'National Stadium', 20, 4, 'Manager'),
-(2, '10:00:00', '15:00:00', 'Aquatics Center', 25, 5, 'Coach');
+(1, '2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium', 20, 4, 'Manager'),
+(2, '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center', 25, 5, 'Coach');
 
 
 INSERT INTO VolunteerStaffs (VolunteerStaffID, EventBeginTimeStamp, EventEndTimeStamp, EventVenueName, HoursWorked, Role)
 VALUES
-(3, '09:00:00', '13:00:00', 'Gymnastics Arena', 4, 'Assistant'),
-(4, '14:00:00', '18:00:00', 'National Stadium', 4, 'Coordinator');
+(3, '2024-07-23 18:00:00', '2024-07-23 20:00:00', 'Gymnastics Arena', 4, 'Assistant'),
+(4, '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'National Stadium', 4, 'Coordinator');
 
 
 INSERT INTO Official (ID, Name, Role, AccommodationName, RoomNumber)
@@ -71,31 +77,25 @@ VALUES
 ('Adidas', 'Canada', 102, 'Silver', '2024-02-10', 300000.00);
 
 
-INSERT INTO Event (BeginTimeStamp, EndTimeStamp, VenueName, SportID)
+INSERT INTO Venue (name, address, maxCapacity)
 VALUES
-('2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium', 101),
-('2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center', 102),
-('2024-07-23 18:00:00', '2024-07-23 20:00:00', 'Gymnastics Arena', 104);
+('National Stadium', '123 Olympic Blvd', 50000),
+('Aquatics Center', '456 Water Way', 15000),
+('Gymnastics Arena', '789 Balance Rd', 10000);
 
 
-INSERT INTO Results (countryName, EventBeginTimeStamp, EventEndTimeStamp, venueName, result)
+INSERT INTO Results (countryName, EventBeginTimeStamp, EventEndTimeStamp, EventVenueName, result)
 VALUES
 ('USA', '2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium', 'Gold'),
 ('Canada', '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center', 'Silver'),
 ('Mexico', '2024-07-23 18:00:00', '2024-07-23 20:00:00', 'Gymnastics Arena', 'Bronze');
 
 
-INSERT INTO Accommodation (AccommodationName, accommodationAddress, maxOccupancy)
+INSERT INTO Accommodation (name, address, maxOccupancy)
 VALUES
 ('Olympic Village', '1 Main St', 500),
 ('Athletes Inn', '2 Athlete Rd', 200);
 
-
-INSERT INTO Venue (venueName, venueAddress, maxCapacity)
-VALUES
-('National Stadium', '123 Olympic Blvd', 50000),
-('Aquatics Center', '456 Water Way', 15000),
-('Gymnastics Arena', '789 Balance Rd', 10000);
 
 
 INSERT INTO Sport (id, sportName)
@@ -119,27 +119,27 @@ VALUES
 ('Athletes Inn', '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center', 'Indoor', 100);
 
 
-INSERT INTO Room (AccommodationName, RoomNumber, maxOccupancy)
+INSERT INTO Room (Number, AccommodationName, maxOccupancy)
 VALUES
-('Olympic Village', 202, 4),
-('Athletes Inn', 105, 2),
-('Olympic Village', 302, 3);
+(202, 'Olympic Village', 4),
+(105, 'Athletes Inn', 2),
+(302, 'Olympic Village', 3);
 
 
-INSERT INTO EventRound (BeginTimeStamp, EndTimeStamp, VenueName, Round)
+INSERT INTO EventRound (EventBeginTimeStamp, EventEndTimeStamp, EventVenueName, Round)
 VALUES
 ('2024-07-21 10:00:00', '2024-07-21 11:00:00', 'National Stadium', 'Quarterfinal'),
 ('2024-07-21 11:30:00', '2024-07-21 12:00:00', 'National Stadium', 'Semifinal'),
 ('2024-07-22 14:00:00', '2024-07-22 15:00:00', 'Aquatics Center', 'Final');
 
 
-INSERT INTO Officiates (OfficialID, BeginTime, EndTime, Venue)
+INSERT INTO Officiates (OfficialID, EventBeginTimeStamp, EventEndTimeStamp, EventVenueName)
 VALUES
 (1, '2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium'),
 (2, '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center');
 
 
-INSERT INTO Competes (CountryName, SportID, BeginTime, EndTime, Venue)
+INSERT INTO Competes (CountryName, SportID, EventBeginTimeStamp, EventEndTimeStamp, EventVenueName)
 VALUES
 ('USA', 101, '2024-07-21 10:00:00', '2024-07-21 12:00:00', 'National Stadium'),
 ('Canada', 102, '2024-07-22 14:00:00', '2024-07-22 16:00:00', 'Aquatics Center'),
@@ -170,7 +170,7 @@ VALUES
 (104, 'Gymnastics');
 
 
-INSERT INTO venueType (venueName, venueType)
+INSERT INTO venueType (venueName, Type)
 VALUES
 ('National Stadium', 'Outdoor'),
 ('Aquatics Center', 'Indoor'),
